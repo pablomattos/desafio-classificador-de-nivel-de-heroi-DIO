05351a57ac299231810b5316b8feb2b6c8289998
Pablo Moraes De Mattos <pablomoraesdemattos@gmail.com>
2023-10-09 20:57:03 -0300

    Adicionando código implementado do desafio DIO

---
+++ classificadorNivelHeroi.js
@@ -0,0 +1,23 @@
+let nome = "DevPablito"
+let xp = 10001
+let nivel = ""
+
+if(xp < 1000){
+    nivel = "Ferro"
+}else if(xp >= 1001 && xp <= 2000){
+    nivel = "Bronze"
+}else if(xp <= 5000){
+    nivel = "Prata"
+}else if(xp >= 6001 && xp <= 7000){
+    nivel = "Ouro"
+}else if(xp <= 8000){
+    nivel = "Platina"
+}else if(xp <= 9000){
+    nivel = "Ascendente"
+}else if(xp <= 10000){
+    nivel = "Imortal"
+}else if(xp >= 10001){
+    nivel = "Radiante"
+}
+
+console.log("O Herói de nome "+nome+" está no nível de "+nivel)
